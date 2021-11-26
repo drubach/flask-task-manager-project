ed96b563f4f712e26397c64a9be97292e7059eb8
drubach <daniel.c.rubach@gmail.com>
2021-11-26 10:36:11 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+Flask Task Manager application with MongoDB
+"""
+
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """Test first method"""
+    return "Hello World ... again!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
