cac0f27a228d6675ddc6cb17d5e7bb6ea2f23e6e
drubach <daniel.c.rubach@gmail.com>
2021-11-26 14:59:32 -0500

    Added protective programming for username & password

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""Main app"""
+
 import os
 from flask import (
     Flask, flash, render_template,
@@ -28,7 +30,25 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register function"""
+    """Registration function"""
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 if __name__ == "__main__":
