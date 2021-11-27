1077dbca2af5389b6d216e62e2c2b568a2733a0c
drubach <daniel.c.rubach@gmail.com>
2021-11-27 18:38:40 -0500

    add category picker to add tasks page

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,9 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $("select").formSelect();
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
@@ -15,6 +16,7 @@ $(document).ready(function () {
         }
     });
 });
+
 
 
 
